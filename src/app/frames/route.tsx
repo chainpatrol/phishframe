096521dac732183dc3b7ad3bd6bc64c532c38b1c
Umar Ahmed <umar@chainpatrol.io>
2024-03-23 15:14:53 -0400

    wip: multi-page

---
+++ src/app/frames/route.tsx
@@ -0,0 +1,41 @@
+import { createFrames, Button } from "frames.js/next";
+
+const totalPages = 5;
+
+const frames = createFrames({
+  basePath: "/frames",
+  initialState: {
+    pageIndex: 0,
+  },
+});
+
+const handleRequest = frames(async (ctx) => {
+  const pageIndex = Number(ctx.searchParams.pageIndex || 0);
+  const nextPageIndex = (pageIndex + 1) % totalPages;
+  const prevPageIndex = (pageIndex - 1 + totalPages) % totalPages;
+
+  const imageUrl = `https://picsum.photos/seed/frames.js-${pageIndex}/300/200`;
+
+  return {
+    image: (
+      <div tw="flex flex-col">
+        <img width={300} height={200} src={imageUrl} alt="Image" />
+        <div tw="flex">
+          This is slide {pageIndex + 1} / {totalPages}
+        </div>
+      </div>
+    ),
+    textInput: "Type something!",
+    buttons: [
+      <Button action="post" target={{ query: { pageIndex: prevPageIndex } }}>
+        ←
+      </Button>,
+      <Button action="post" target={{ query: { pageIndex: nextPageIndex } }}>
+        →
+      </Button>,
+    ],
+  };
+});
+
+export const GET = handleRequest;
+export const POST = handleRequest;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 import Image from "next/image";
 
+import { fetchMetadata } from "frames.js/next";
+
+export async function generateMetadata() {
+  return {
+    title: "Frames",
+    // provide a full URL to your /frames endpoint
+    other: await fetchMetadata(
+      new URL(
+        "/frames",
+        process.env.VERCEL_URL
+          ? `https://${process.env.VERCEL_URL}`
+          : "http://localhost:3000"
+      )
+    ),
+  };
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
