b175124736a0ee658b004a258c963c255fdf4ffd
Umar Ahmed <umar@chainpatrol.io>
2024-03-24 15:03:06 -0400

    wip: move fonts

--- src/app/frames/route.tsx
+++ src/app/frames/route.tsx
@@ -16,12 +16,7 @@ const chainpatrol = new ChainPatrolClient({
 });
 
 async function loadFont(file: string) {
-  const url = new URL(
-    process.env.VERCEL
-      ? `../assets/fonts/${file}`
-      : `../../../public/assets/fonts/${file}`,
-    import.meta.url
-  );
+  const url = new URL(`../../../fonts/${file}`, import.meta.url);
   const filepath = fileURLToPath(url);
   console.log({ file, url: url.toString(), filepath });
   // const res = await fetch(url);
