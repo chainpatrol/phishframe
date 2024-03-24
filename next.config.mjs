aa0b0d6475863d551221f54caaf7c58d3df6a439
Umar Ahmed <umar@chainpatrol.io>
2024-03-24 09:49:46 -0400

    wip: try transpiling package

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import analyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  optimizeFonts: true,
+  reactStrictMode: true,
+  transpilePackages: ["@farcaster/core", "frames.js"],
   experimental: {
     optimizePackageImports: ["@farcaster/core", "frames.js"],
   },
